040f559503d4db7b45aeb1d86c71cf0edac74cc4
steven davey <daveysj@gmail.com>
2024-04-03 13:46:34 +0200

    add requirements to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    """Parse a requirements file into a list of dependencies."""
+    with open(filename, 'r') as file:
+        requirements = file.readlines()
+    requirements = [r.strip() for r in requirements]
+    # Exclude specific lines such as comments or empty lines
+    requirements = [r for r in requirements if r and not r.startswith('#')]
+    # Here you can also add logic to handle package versioning more gracefully
+    return requirements
+
+# Usage
+requirements = parse_requirements('requirements.txt')
+
 setup(
     name='regulations_rag',
     version='0.1',
     package_dir={'': 'src'},  # Specifies that packages are under src directory
     packages=find_packages(where='src'),  # Tells setuptools to find packages under src
+    install_requires=requirements,
 )
