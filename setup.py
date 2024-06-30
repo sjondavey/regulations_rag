68cf1d846bb06b979390feb91343fee389beb8db
steven davey <daveysj@gmail.com>
2024-06-30 13:35:08 +0200

    workflow

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = parse_requirements('requirements.txt')
 
 setup(
     name='regulations_rag',
-    version='0.6.3.16',
+    version='0.6.3.17',
     packages=find_packages(),  # Automatically find and include packages
     install_requires=requirements,
     include_package_data=True,  # Ensure package data files are included
